36acecc7b4c86df6b13e9e90778cef1b70d2b583
Kdohyoung <asd62126310@gmail.com>
2022-05-31 11:12:27 +0900

    Update app_ml.py

--- app_ml.py
+++ app_ml.py
@@ -10,7 +10,8 @@ from PIL import Image
 
 def run_ml() :
     df=pd.read_csv('data/hotels.csv',index_col=0)
-    st.subheader('당신이 선택한 호텔과 유사한 호텔 추천')
+    st.subheader('당신이 선택한 호텔과 유사한 호텔 5개 추천')
+    st.text('숫자가 1에 가까울수록 비슷한 호텔입니다')
     df2=df.pivot_table(values='reviews.rating',index='reviews.username',columns='name',aggfunc='mean')
     all_corr = df2.corr(min_periods=5)
     hotels = df['name'].unique()
@@ -19,4 +20,4 @@ def run_ml() :
     recom_hotels=all_corr[hotels_name].dropna()
     recom_hotels.columns=['correlation']
     recom_hotels = recom_hotels.sort_values(ascending=False).to_frame()
-    st.dataframe(recom_hotels)
+    st.dataframe(recom_hotels.head())
